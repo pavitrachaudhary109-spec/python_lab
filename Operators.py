1c683aa711c922ac355e21ba8b2df7569eb58f65
pavitrachaudhary109-spec <pavitrachaudhary109@gmail.com>
2025-12-29 10:52:57 +0530

    Add files via upload
    All operators

---
+++ Operators.py
@@ -0,0 +1,49 @@
+ #Assignment Operator
+ x=10
+print(x)
+
+
+#urnary minus
+a=5
+print(-a) 
+
+#relational
+p=10
+q=5
+print(p>q)
+print(p==q)
+
+
+#Logical
+x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x)
+
+# boolean 
+is_pass= True
+print(is_pass)
+
+#bitwise
+a= 5
+b= 3
+print(a & b)
+print(a | b)
+
+#membership 
+number = [1,2,3,4,5,6]
+print(2 in number)
+print(5 not in number)
+
+#identity
+x= 10
+y= 10
+print(x is y )
+print(x isnot y)
+
+#Arithmetic 
+a=5
+b=10
+print(a+b)
+
