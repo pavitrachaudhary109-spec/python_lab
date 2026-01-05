eb74bb7387a68ed9c0a618382f76190aa37f47c8
pavitrachaudhary109-spec <pavitrachaudhary109@gmail.com>
2026-01-05 10:25:56 +0530

    Add files via upload
    Data Types in Python

---
+++ data_type.py
@@ -0,0 +1,25 @@
+x="HELLO World" #str
+print(x)
+
+y= 20 #int
+print(y)
+
+z= 20.5 #float
+print(z)
+
+a=1j #complex
+print(a)
+
+b=["apple","banana","cherry"] #list
+print(b)
+
+c=("Apple","banana","cherry") #tuple
+print(c)
+
+d={"name":"John","Age":"36"}#dict
+print(d)
+
+e="True" #bool
+print(e)
+
+f=None
