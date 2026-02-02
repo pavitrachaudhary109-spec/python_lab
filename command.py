be03a5d3fcb30d659dd5244aa3b6ad5279d4c4fa
pavitrachaudhary109-spec <pavitrachaudhary109@gmail.com>
2026-02-02 10:44:10 +0530

    Add files via upload

---
+++ command.py
@@ -0,0 +1,18 @@
+import sys
+print(sys.argv)
+
+"""String 
+D:\Python> python command.py Pavitra chaudhary
+['command.py', 'Pavitra', 'chaudhary']
+
+Integer
+PS D:\Python> python command.py 25 45 66         
+['command.py', '25', '45', '66']
+
+Float
+PS D:\Python> python command.py 25.4 9.85 7.66
+['command.py', '25.4', '9.85', '7.66']
+
+bool
+PS D:\Python> python command.py True          
+['command.py', 'True']"""
