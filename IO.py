9bc14fab90feb0df17d2694d53dbe184218d14de
pavitrachaudhary109-spec <pavitrachaudhary109@gmail.com>
2026-02-02 10:32:10 +0530

    Add files via upload

---
+++ IO.py
@@ -0,0 +1,29 @@
+a=int(input("Enter Your Number:  "))
+print(a)
+
+x=int(input("Enter Your Number:  "))
+print(x)
+
+
+b=input("Enter Your Name:  ")
+print(b)
+
+y=input("Enter Your Name:  ")
+print(y)
+
+c=float(input("Enter your CGPA: "))
+print(c)
+
+z=float(input("Enter your SGPA: "))
+print(z)
+
+d=input("Enter a number: ")
+e=input("Enter a number: ")
+print(d<e)
+print(d>e)
+
+f=input("Enter a number: ")
+g=input("Enter a number: ")
+print(f==g)
+print(f>g)
+
