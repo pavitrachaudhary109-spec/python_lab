e75ab9c60bd18870d3bbdc6db378309e0cf4f410
pavitrachaudhary109-spec <pavitrachaudhary109@gmail.com>
2026-02-11 09:05:37 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number: "))
+fact = 1
+
+if num < 0:
+    print("Factorial does not exist for negative numbers")
+elif num == 0:
+    print("Factorial is 1")
+else:
+    for i in range(1, num + 1):
+        fact = fact * i
+
+    print("Factorial of", num, "is", fact)
