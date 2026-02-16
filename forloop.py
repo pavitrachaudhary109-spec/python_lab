54d539576c5e9ff3fe4d02ec6d1c8f98b033a892
pavitrachaudhary109-spec <pavitrachaudhary109@gmail.com>
2026-02-16 10:26:03 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,44 @@
+#print numbers 1 to 5
+for i in range(1,6):
+  print(i)
+
+#Print message 3 times
+for i in range(3):
+    print("Hello")
+
+#print numbers from 1 to 10
+for i in range(1,11):
+    print(i)
+
+#print even number from 1 to 20
+
+for i in range(1,21):
+    if(i%2==0):
+        print(i)
+
+#print odd number from 1 to 15
+for i in range(1,16):
+    if(i%2!=0):
+        print(i)
+
+#print table of 5
+for i in range(1,11):
+    print("5 x ",i,"=",5*i)
+
+#print characters of string
+
+name= "Atmiya"
+for letter in name:
+    print(letter)
+
+#Sum of number from 1 to  5
+total=0
+for i in range(1,6):
+    total+=i
+print(" Sum is :",total)
+
+#print list elements
+
+numbers=[10,20,30]
+for i in numbers:
+    print(n)
